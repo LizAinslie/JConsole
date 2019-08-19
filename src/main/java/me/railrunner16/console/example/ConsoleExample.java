a1e673a4018ce224b757462c8b665160c4321cb2
RailRunner16 <railinator4903@gmail.com>
2019-08-19 09:32:32 -0500

    add example file in source

---
+++ src/main/java/me/railrunner16/console/example/ConsoleExample.java
@@ -0,0 +1,14 @@
+package me.railrunner16.console.example;
+
+import me.railrunner16.console.Console;
+
+public class ConsoleExample {
+	public static void main(String[] args) {
+		Console console = new Console(20, "Welcome!");
+		console.run();
+
+		String s = console.promptString("Test String: ");
+
+		System.out.println(s);
+	}
+}
